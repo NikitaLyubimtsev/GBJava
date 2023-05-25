867ebf91fabba8d3610da18bb520af18fe4dd774
NikitaLyubimtsev <lubick89@gmail.com>
2023-05-25 09:22:06 +0300

    Seminar6 Added

---
+++ src/main/java/org/example/Seminar6.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Seminar6 {
+    public static void main(String[] args) {
+
+        mySet mySet = new mySet();
+        mySet.getInt();
+
+    }
+}
+
+class mySet {
+    int a;
+    String b;
+
+    public int getInt() {
+        return 4;
+    }
+}
